5351b509b4e39a7c742180df2f235da49c13f626
meu2100 <amanda8018@gmail.com>
2024-06-29 12:23:29 +0800

    router init"

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res)=>
-  res.send("Hello word")
-)
+app.get("/", (req, res) => res.send("Hello word"));
 
-app.listen(port, () =>{
+app.get("/todos", (req, res) => {
+  res.send("get all todos");
+});
+
+app.get("/todos/new", (req, res) => {
+  res.send("create todo");
+});
+
+app.post("/todos", (req, res) => {
+  res.send("add todo");
+});
+
+app.get("/todos/:id", (req, res) => {
+  res.send(`get todo: ${req.params.id}`);
+});
+
+app.get("/todos/:id/edit", (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`);
+});
+
+app.put("/todos/:id", (req, res) => {
+  res.send("modify todo");
+});
+
+app.delete("/todos/:id", (req, res) => {
+  res.send("delete todo");
+});
+
+app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
-})
+});
