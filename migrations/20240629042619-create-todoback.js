d432171f8269541e77a177a9e397e1baf7dc0d14
meu2100 <amanda8018@gmail.com>
2024-06-29 12:56:48 +0800

    modify todo migration file

--- migrations/20240629042619-create-todoback.js
+++ migrations/20240629042619-create-todoback.js
@@ -1,28 +1,33 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Todobacks', {
+    await queryInterface.createTable("Todobacks", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Todobacks');
-  }
+    await queryInterface.dropTable("Todobacks");
+  },
 };
