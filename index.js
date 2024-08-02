6b1d9804721f7448eaa9c2619cc5b4ea196300fe
meu2100 <amanda8018@gmail.com>
2024-08-02 21:25:19 +0800

    implement login using passport

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require("./routes");
+const passport = require("passport");
 const port = 3000;
 
 const { engine } = require("express-handlebars");
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.use(router);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
